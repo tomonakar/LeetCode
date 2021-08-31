e23d5c779fc38ecf6773e05bb08a94baab38e1da
tomonakar <tomohisa.nak@gmail.com>
2021-08-31 09:46:52 +0900

    feat: 2. add-two-numbers

--- 2.add-two-numbers.go
+++ 2.add-two-numbers.go
@@ -13,7 +13,21 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    
+	carry, dummy := 0, new(ListNode)
+	for node := dummy; l1 != nil || l2 != nil || carry > 0; node = node.Next {
+		if l1 != nil {
+			carry += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			carry += l2.Val
+			l2 = l2.Next
+		}
+		node.Next = &ListNode{Val: carry % 10, Next: nil}
+		carry /= 10
+	}
+	return dummy.Next
 }
+
 // @lc code=end
 
