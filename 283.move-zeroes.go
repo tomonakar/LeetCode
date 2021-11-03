d149c15e23192dffde92ce4e618aa17a71357419
tomonakar <tomohisa.nak@gmail.com>
2021-11-04 08:10:59 +0900

    feat: 283. two pointers

---
+++ 283.move-zeroes.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=283 lang=golang
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+func moveZeroes(nums []int) {
+	nonZeroIndex := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[i], nums[nonZeroIndex] = nums[nonZeroIndex], nums[i]
+			nonZeroIndex++
+		}
+	}
+}
+
+// @lc code=end
+
